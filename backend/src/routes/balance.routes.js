16d0a7d0f11d4e0fb683419d6826b60d8cbee2b5
SMoralesG <sergio.morales.gu@gmail.com>
2025-07-06 16:36:59 -0400

    pequeños cambios. Solo lo hice para pullear el main

--- backend/src/routes/balance.routes.js
+++ backend/src/routes/balance.routes.js
@@ -1,6 +1,5 @@
 "use strict";
 import { Router } from "express";
-import { isAdmin } from "../middlewares/authorization.middleware.js";
 import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 import {
   getAllBalances,
@@ -13,7 +12,6 @@ const router = Router();
 
 router
   .use(authenticateJwt)
-  .use(isAdmin);
 
 router
   .get("/", getAllBalances)
